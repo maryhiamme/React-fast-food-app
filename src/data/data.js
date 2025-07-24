f7a97b8d4cbac26017a4a0f2a191acb0145c623d
Disu mariam omobolanle <disu15010811131@gmail.com>
2025-07-24 16:21:05 +0100

    react fast food app files amd assests

---
+++ src/data/data.js
@@ -0,0 +1,192 @@
+import fastFoodIcon from '../assets/drink_16475361 2.png'
+import chickenChipsIcon from '../assets/fast-food_1585309.png'
+import pizzaIcon from '../assets/pizza_2816823.png'
+import medalIcon from '../assets/medal_2583276.png'
+import pancakeIcon from '../assets/pancakes_7450750.png'
+import ratingIcon from '../assets/rating_3913800.png'
+import deliveryIcon from '../assets/fast-shipping_17627501.png'
+import chickenLegIcon from '../assets/chicken-leg_816185.png'
+
+export const data = [
+    {
+      id: 1,
+      name: 'Barbecue wing',
+      category: 'wing',
+      image:
+        'https://images.unsplash.com/photo-1624726175512-19b9baf9fbd1?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZHN8ZW58MHx8MHx8fDA%3D',
+      price: '$6',
+    },
+    {
+      id: 2,
+      name: 'Spicy wings',
+      category: 'wing',
+      image:
+        'https://images.unsplash.com/photo-1734987942068-a1a459d65d3d?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3BpY3klMjBjaGlja2VuJTIwd2luZ3N8ZW58MHx8MHx8fDA%3D',
+      price: '$4',
+    },
+    {
+      id: 3,
+      name: 'Raspberry pancake',
+      category: 'pancake',
+      image:
+        'https://images.unsplash.com/photo-1528207776546-365bb710ee93?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGFuY2FrZXN8ZW58MHx8MHx8fDA%3D',
+      price: '$4',
+    },
+    {
+      id: 4,
+      name: 'Banana pancake',
+      category: 'pancake',
+      image:
+        'https://images.unsplash.com/photo-1606149186228-4e5ac94a742e?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmFuYW5hJTIwcGFuY2FrZXxlbnwwfHwwfHx8MA%3D%3D',
+      price: '$6',
+    },
+    {
+      id: 5,
+      name: 'Chicken suya pizza',
+      category: 'pizza',
+      image:
+        'https://images.unsplash.com/photo-1571407970349-bc81e7e96d47?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fHBpenphfGVufDB8fDB8fHww',
+      price: '$10',
+    },
+    {
+      id: 6,
+      name: 'Spicy Pizza',
+      category: 'pizza',
+      image:
+        'https://images.unsplash.com/photo-1576458088443-04a19bb13da6?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHBpenphfGVufDB8fDB8fHww',
+      price: '$10',
+    },
+    {
+      id: 7,
+      name: 'Pepperoni Lovers',
+      category: 'pizza',
+      image:
+        'https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVwcGVyb25pJTIwcGl6emF8ZW58MHx8MHx8fDA%3D',
+      price: '$12',
+    },
+    {
+      id: 8,
+      name: 'Cheese Pizza',
+      category: 'pizza',
+      image:
+        'https://images.unsplash.com/photo-1548369937-47519962c11a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2hlZXNlJTIwcGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+      price: '$6',
+    },
+    {
+      id: 9,
+      name: 'Chocolate pancake',
+      category: 'pancake',
+      image:
+        'https://images.unsplash.com/photo-1668507740203-0654d38b6201?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2hvY2xhdGUlMjBwYW5jYWtlfGVufDB8fDB8fHww',
+      price: '$6',
+    },
+    {
+      id: 10,
+      name: 'Stawberry pancake',
+      category: 'pancake',
+      image:
+        'https://images.unsplash.com/photo-1614663422784-07203a605793?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3RyYXdiZXJyeSUyMHBhbmNha2V8ZW58MHx8MHx8fDA%3D',
+      price: '$6',
+    },
+    {
+      id: 11,
+      name: 'Crispy chicken and fries',
+      category: 'chicken',
+      image:
+        'https://images.unsplash.com/photo-1716235598066-27877bd4215d?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hpY2tlbiUyMGFuZCUyMGNoaXBzfGVufDB8fDB8fHww',
+      price: '$6',
+    },
+    {
+      id: 12,
+      name: 'Fruit Salad',
+      category: 'salad',
+      image:
+        'https://images.unsplash.com/photo-1564093497595-593b96d80180?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZnJ1aXQlMjBzYWxhZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+      price: '$4',
+    },
+    {
+      id: 13,
+      name: 'Wings',
+      category: 'wing',
+      image:
+        'https://images.unsplash.com/photo-1567620832903-9fc6debc209f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+      price: '$4',
+    },
+    {
+      id: 14,
+      name: 'Baked Chicken',
+      category: 'chicken',
+      image:
+        'https://images.unsplash.com/photo-1594221708779-94832f4320d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+      price: '$12',
+    },
+    {
+      id: 15,
+      name: 'Chicken Tenders',
+      category: 'chicken',
+      image:
+        'https://images.unsplash.com/photo-1562967914-608f82629710?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNoaWNrZW4lMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+      price: '$10',
+    },
+    {
+      id: 16,
+      name: 'Chicken Kabob',
+      category: 'chicken',
+      image:
+        'https://images.unsplash.com/photo-1603360946369-dc9bb6258143?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNoaWNrZW4lMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+      price: '$12',
+    },
+  ];
+  
+  export const categories = [
+    {
+      id: 1,
+      name: 'Fast Food',
+      image: fastFoodIcon
+        ,
+        
+    },
+    {
+      id: 2,
+      name: 'Pizza',
+      image:
+        pizzaIcon,
+    },
+    {
+      id: 3,
+      name: 'Wings',
+      image:
+        chickenLegIcon,
+    },
+    {
+      id: 4,
+      name: 'Pancakes',
+      image:
+        pancakeIcon,
+    },
+    {
+      id: 5,
+      name: 'Latest Deals',
+      image:
+        chickenChipsIcon,
+    },
+    {
+      id: 6,
+      name: 'Restaurant Rewards',
+      image:
+       ratingIcon ,
+    },
+    {
+      id: 7,
+      name: 'Best Overall',
+      image:
+        medalIcon,
+    },
+    {
+      id: 8,
+      name: 'Shipped Free',
+      image:
+        deliveryIcon,
+    },
+  ];
+  
